13232dbc93811f922487cb2f0aec7fbf3f04793b
sergeymaguire <sergeymaguire@gmail.com>
2019-03-05 13:11:44 -0800

    integer comparisons using boolean

--- cSharpMakingDecisions/Program.cs
+++ cSharpMakingDecisions/Program.cs
@@ -11,27 +11,58 @@ namespace cSharpMakingDecisions
         static void Main(string[] args)
         {
 
-            const int HIGH = 10, LOW = 5;
-            string numberString;
-            int number;
-            Console.Write("Enter a number: ");
-            numberString = Console.ReadLine();
-            number = Convert.ToInt32(numberString);
-            integerDecison(HIGH, LOW, number);
+           // const int HIGH = 10, LOW = 5;
+           //// int number;
+            //Console.Write("Enter a number: ");
+            //numberString = Console.ReadLine();
+            //number = Convert.ToInt32(numberString);
+            //integerDecison(HIGH, LOW, number);
+            string numbersString;
+            int counter = 1;
+            int num1, num2, num3;
+            Console.Write("Enter a integer: #{0} ", counter++);
+            numbersString = Console.ReadLine();
+            num1 = Convert.ToInt32(numbersString);
+            Console.Write("Enter a integer: #{0} ", counter++);
+            numbersString = Console.ReadLine();
+            num2 = Convert.ToInt32(numbersString);
+            Console.Write("Enter  integer: #{0} ",counter++);
+            numbersString = Console.ReadLine();
+            num3 = Convert.ToInt32(numbersString);
+            if (num1 == num2)
+            {
+                Console.WriteLine("the first number {0} and the second number {1} are equal", num1, num2);
+                Console.ReadLine();
+            }
+            else if (num2 == num3)
+            {
+                Console.WriteLine("the second number {0} and last number {1} are equal", num2, num3);
+                Console.ReadLine();
+            } else if(num1 == num3) {
+                Console.WriteLine("The first number {0} and last number {1} are equal", num1, num3);
+                Console.ReadLine();
+            }else
+            {
+                Console.WriteLine("No two numbers are equal");
+                Console.ReadLine();
+            }
+               
+            
         }
 
-        private static void integerDecison(int HIGH, int LOW, int number)
-        {
-            if (number < HIGH)
-            {
-                Console.WriteLine("{0} is between {1} and {2}.", number, LOW, HIGH);
-                Console.ReadLine();
-            }
-            else if (number >= HIGH)
-            {
-                Console.WriteLine("{0} is not between {1} and {2}.", number, LOW, HIGH);
-                Console.ReadLine();
-            }
-        }
+        //private static void integerDecison(int HIGH, int LOW, int number)
+       // {
+           // if (number < HIGH)
+          //  {
+             //   Console.WriteLine("{0} is between {1} and {2}.", number, LOW, HIGH);
+             //   Console.ReadLine();
+          //  }
+           // else if (number >= HIGH)
+           // {
+             //   Console.WriteLine("{0} is not between {1} and {2}.", number, LOW, HIGH);
+               // Console.ReadLine();
+           // }
+       // }
+      
     }
 }
